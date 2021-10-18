3312d33cfb808843fd2b50fc959aacf46d4bcd2a
chaudeveloper <ms_michanguyen@yahoo.com>
2021-10-18 13:34:00 -0400

    adding week 2 folder

---
+++ Week1/java-practise/watch-that-box/jvascript.js
@@ -0,0 +1,23 @@
+document.getElementById("button1").addEventListener("click", function(){
+
+    document.getElementById("box").style.height = "250px";
+
+});
+
+document.getElementById("button2").addEventListener("click", function(){
+
+    document.getElementById("box").style.backgroundColor= "blue";
+
+});
+
+document.getElementById("button3").addEventListener("click", function(){
+
+    document.getElementById("box").style.opacity= "20%";
+
+});
+
+document.getElementById("button4").addEventListener("click", function(){
+
+    document.getElementById("box").style="height:150px; width:150px; background-color:orange; margin:25px"
+
+});
